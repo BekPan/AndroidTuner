18a6a872c8f20c39ed320de9166629f184415071
panos <panosb00@gmail.com>
2023-04-19 17:24:02 +0300

    Changed layout

--- app/src/main/java/com/bekiarispanagiotis/androidtuner/MainActivity.java
+++ app/src/main/java/com/bekiarispanagiotis/androidtuner/MainActivity.java
@@ -4,9 +4,6 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
@@ -33,13 +30,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
-        WindowInsetsControllerCompat windowInsetsController =
-                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
-        if (windowInsetsController == null) {
-            return;
-        }
-        windowInsetsController.setAppearanceLightNavigationBars(true);
         setContentView(R.layout.activity_main);
 
         permissionResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
